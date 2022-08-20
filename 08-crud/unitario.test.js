d670f6e9d43ba8dec4dc480233fd143085f9f3bd
Arquimedes Garcia <arquimedesallan02@gmail.com>
2022-08-20 18:35:22 -0300

    clniciado o teste da função updateClient

--- 08-crud/unitario.test.js
+++ 08-crud/unitario.test.js
@@ -40,11 +40,43 @@ describe('CRUD', () => {
 
         const saveClient = await page.evaluate(() => saveClient());
         
-        await page.waitForTimeout(1000)
+        await page.waitForTimeout(1000);
 
         const readClient = await page.evaluate(() => readClient().length);
 
         await expect(readClient).toBe(1);
     });
+
+    it('ao chamar a função "readClient" ela deve retornar uma array contendo tamanho maior que 0, neste caso de teste ', async () => {
+        const nome = await page.evaluate(() => document.getElementById('nome').value = "José da silva");
+        const email = await page.evaluate(() => document.getElementById('email').value = "josedasilva@email.com");
+        const celular = await page.evaluate(() => document.getElementById('celular').value = "00 0000-0000");
+        const cidade = await page.evaluate(() => document.getElementById('cidade').value = "Lugar Nenhum-MG");
+
+        const saveClient = await page.evaluate(() => saveClient());
+
+        await page.waitForTimeout(1000);
+
+        const readClient = await page.evaluate(() => readClient().length);
+
+        expect(readClient).toBeGreaterThan(0);
+    });
+
+    it('função "updateClient"', async () => {
+        const nome = await page.evaluate(() => document.getElementById('nome').value = "José da silva");
+        const email = await page.evaluate(() => document.getElementById('email').value = "josedasilva@email.com");
+        const celular = await page.evaluate(() => document.getElementById('celular').value = "00 0000-0000");
+        const cidade = await page.evaluate(() => document.getElementById('cidade').value = "Lugar Nenhum-MG");
+        
+        const saveClient = await page.evaluate(() => saveClient());
+
+        await page.waitForTimeout(1000);
+
+        const clientModificado = await page.evaluate(() => document.getElementById('tableClient'));
+
+        const updateClient = await page.evaluate(() => updateClient(0, clientModificado));
+
+        expect(clientModificado.nome).toMatch("Maria da Silva");
+    })
 
 });
